02bda255258e022481754cb6459170a4a24b7f4a
Boost <boostbm@gmail.com>
2022-02-17 16:22:09 -0800

    Updated JS

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
-const addTodo = document.getElementById('addTodo');
+const addTodoButton = document.getElementById('addTodo');
 const toDoContainer = document.getElementById('toDoContainer');
 const inputfield = document.getElementById('inputfield');
+
+addTodoButton.addEventListener('click', function(){
+  var paragraph = document.createElement('p');
+  paragraph.classList.add('paragraph-styling');
+  paragraph.innerText = inputfield.value;
+  toDoContainer.appendChild(paragraph);
+  inputfield.value = "";
+  paragraph.addEventListener('click', function(){
+    paragraph.style.textDecoration = 'line-through';
+  })
+  paragraph.addEventListener('dblclick', function(){
+    toDoContainer.removeChild(paragraph);
+  })
+})
+
